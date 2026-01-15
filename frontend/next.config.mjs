5788bd63ac8fde8d1226b10ac7926f3c1b0fbe14
Daniel Radovanovic <dev@danielradovanovic.ch>
2026-01-15 19:47:17 +0100

    fix apiClient

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isDev = process.env.NODE_ENV === 'development';
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/v1/:path*',
-        destination: 'https://solarspotting-app-production.up.railway.app/api/v1/:path*',
+        destination: isDev
+          ? 'http://localhost:8000/api/v1/:path*'
+          : 'https://solarspotting-app-production.up.railway.app/api/v1/:path*',
       },
     ];
   },
